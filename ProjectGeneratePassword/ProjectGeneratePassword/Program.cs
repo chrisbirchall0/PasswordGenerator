4c351e25bd7e96d2354658a279aa5d6e2e6e4648
Chris Birchall <christopher.birchall@ao.com>
2023-03-21 14:21:04 +0000

    feat: create password

---
+++ ProjectGeneratePassword/ProjectGeneratePassword/Program.cs
@@ -0,0 +1,23 @@
+﻿using PasswordGenerator;
+using System;
+using TextCopy;
+
+namespace ProjectGeneratePassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // Password construction
+            var pwd = new Password(includeLowercase: true, includeUppercase: true, includeNumeric: true, includeSpecial: true, passwordLength: 16);
+            // Get password as variable
+            var password = pwd.Next();
+            // Uses 'TextCopy' Nuget to set clipboard content with generated password
+            TextCopy.ClipboardService.SetText(password);
+            // Print password in CMD
+            Console.WriteLine(password);
+            // Hold password in CMD
+            Console.Read(); 
+        }
+    }
+}
